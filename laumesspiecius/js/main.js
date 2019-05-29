becdc056a917318d6095db783d0900f3ea2974f0
Tdot94 <51154966+Tdot94@users.noreply.github.com>
2019-05-29 13:52:42 +0300

    Add files via upload

---
+++ laumesspiecius/js/main.js
@@ -0,0 +1,25 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top 
+      },
+      800
+    );
+  }
+});
